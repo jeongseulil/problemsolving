0569c02762195436b8dce21b17fe30facdfc95f8
jeongseulil <jong6114s@gmail.com>
2024-11-05 20:05:38 +0900

    lottery 1000

---
+++ problemsolving.cpp/problemsolving.cpp/Lottery.cpp
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//int main()
+//{
+//	
+//	int pickedindex[6];
+//	int Current = 0;
+//	
+//		while(Current < 6)
+//	{
+//		bool balreadypicked = false;
+//		int lottery = rand() % 45 + 1;
+//		for (int j = 0; j < Current; j++)
+//		{
+//			if()
+//
+//			{
+//				balreadypicked = true;
+//				
+//			}
+//		}
+//		if () {
+//
+//	}
+//	}
+//}
